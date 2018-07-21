3e8e5e54068267685079413436ee25259ab354f3
Victor Alonso <vacontrerasmeza@gmail.com>
2018-07-20 20:49:34 -0500

    Arrays

--- 2-JS-Basic/script.js
+++ 2-JS-Basic/script.js
@@ -288,7 +288,7 @@ yearsUntilRetirement(1969, 'Jane');
 
 /*****************************************
  * Function Statements and Expressions
- */
+
 
 // Function declaration
 // function whatDoYouDo(job, firstName){};
@@ -310,3 +310,42 @@ var whatDoYouDo = function(job, firstName){
 console.log(whatDoYouDo('teacher', 'John'));
 console.log(whatDoYouDo('designer', 'Jane'));
 console.log(whatDoYouDo('retired', 'Mark'));
+
+*/
+
+/***************************************
+ * Arrays
+ */
+
+ // Initialize new array
+var names = ['John', 'Mark', 'Jane'];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names[2]);
+console.log(names.length);
+
+// Mutate array data
+names[1] = 'Ben';
+names[names.length] = 'Mary';
+console.log(names);
+
+// Different data types
+
+var john = ['John', 'Smith', 1990, 'designer', false];
+// Add an element to the end of the array
+john.push('blue');
+// Add an element to the start of the array
+john.unshift('Mr.')
+console.log(john);
+
+//Remove an element at the end of the array
+john.pop();
+john.pop();
+// Remove an element at the start of the array
+john.shift();
+console.log(john);
+
+console.log(john.indexOf(1990));
+
+var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John IS a designer';
+console.log(isDesigner);
