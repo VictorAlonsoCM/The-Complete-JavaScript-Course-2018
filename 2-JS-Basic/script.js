9f0502907c33f6d2ac104eb4408dd7798ba177ee
Victor Alonso <vacontrerasmeza@gmail.com>
2018-07-20 20:34:25 -0500

    Function Statements and Expressions

--- 2-JS-Basic/script.js
+++ 2-JS-Basic/script.js
@@ -259,7 +259,7 @@ if(JohnScored > MikeScored && JohnScored > MaryScored){
 
 /***************************************************
  * Functions
- */
+
 
 function calculateAge(birthYear){
     return 2018 - birthYear;
@@ -283,3 +283,30 @@ function yearsUntilRetirement(year, firstName){
 yearsUntilRetirement(1990, 'John');
 yearsUntilRetirement(1948, 'Mike');
 yearsUntilRetirement(1969, 'Jane');
+
+ */
+
+/*****************************************
+ * Function Statements and Expressions
+ */
+
+// Function declaration
+// function whatDoYouDo(job, firstName){};
+
+ // Function expression
+var whatDoYouDo = function(job, firstName){
+    switch(job){
+        case 'teacher':
+            return firstName + ' teacher kids how to code.';
+        case 'driver':
+            return firstName + ' drives a cab in Lisbon.';
+        case 'designer':
+            return firstName + ' designs beautiful websites';
+        default:
+            return firstName + ' does something else';
+    };
+};
+
+console.log(whatDoYouDo('teacher', 'John'));
+console.log(whatDoYouDo('designer', 'Jane'));
+console.log(whatDoYouDo('retired', 'Mark'));
