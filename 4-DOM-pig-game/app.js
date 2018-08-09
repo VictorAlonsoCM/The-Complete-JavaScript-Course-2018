89d7eac0a8edf054c6417ead24d44baecb79544c
Victor Alonso <vacontrerasmeza@gmail.com>
2018-08-09 12:17:04 -0500

    Finishing Touches: State Variables

--- 4-DOM-pig-game/app.js
+++ 4-DOM-pig-game/app.js
@@ -4,12 +4,12 @@ GAME RULES:
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
 
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 
 //This is for initializing the game
 init();
@@ -17,37 +17,44 @@ init();
 // Animating the dice with the button btn-roll
 document.querySelector('.btn-roll').addEventListener('click', () => {
     // This an Anonymous Function and a Callback that will be called when the button were pressed
-    //1. Random number
-    var dice = Math.floor(Math.random()*6)+1;
-    //2. Display the result
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
-    //3. Update the round score IF the rolled number was NOT a 1
-    if(dice !== 1){
-        //Add score
-        roundScore += dice;
-        document.querySelector("#current-" + activePlayer).textContent = roundScore;
-    }else{
-        nextPlayer();
+    if(gamePlaying){
+        //1. Random number
+        var dice = Math.floor(Math.random()*6)+1;
+        //2. Display the result
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+        //3. Update the round score IF the rolled number was NOT a 1
+        if(dice !== 1){
+            //Add score
+            roundScore += dice;
+            document.querySelector("#current-" + activePlayer).textContent = roundScore;
+        }else{
+            nextPlayer();
+        }
     }
 });
 
 document.querySelector('.btn-hold').addEventListener('click', () =>{
-    //Add CURRENT score to GLOBAL score
-    scores[activePlayer] += roundScore;
+    
+    if(gamePlaying){
+        //Add CURRENT score to GLOBAL score
+        scores[activePlayer] += roundScore;
 
-    // Update the UI
-    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-    // Check if payer won the game
-    if(scores[activePlayer] >= 100){
-        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-        document.querySelector('.dice').style.display = 'none';
-        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-    }else{
-        //Next player
-        nextPlayer();
+        // Update the UI
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+        
+        // Check if payer won the game
+        if(scores[activePlayer] >= 100){
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+        }else{
+            //Next player
+            nextPlayer();
+        }
     }
 });
 
@@ -82,6 +89,7 @@ function init(){
     scores = [0,0];
     roundScore = 0;
     activePlayer = 0;
+    gamePlaying = true;
 
     //This just write everything as a string
     //document.querySelector('#current-' + activePlayer).textContent = dice;
