ed5b68f2bbe08469caeca54a6b0bae95d105c91b
Victor Alonso <vacontrerasmeza@gmail.com>
2018-11-02 16:31:23 -0600

    Closures

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -170,12 +170,46 @@ function game(){
 
 game(); */
 
-(function () {
-  var score = Math.random() * 10;
-  console.log(score >= 5);
-})();
+// (function () {
+//   var score = Math.random() * 10;
+//   console.log(score >= 5);
+// })();
 
-(function (goodLuck) {
-  var score = Math.random() * 10;
-  console.log(score >= 5 - goodLuck);
-})(5);
+// (function (goodLuck) {
+//   var score = Math.random() * 10;
+//   console.log(score >= 5 - goodLuck);
+// })(5);
+
+/******************************** */
+// Closures
+/******************************** */
+
+// function retirement(retirementAge){
+//   var a = ' Years left until retirement';
+//   return function(yearOfBirth){
+//     var age = 2018 - yearOfBirth;
+//     console.log((retirementAge - age) + a);
+//   }
+// }
+
+// var retirementUS = retirement(66);
+// retirementUS(1994);
+
+// retirement(66)(1990);
+
+// var retirementGermany = retirement(65);
+// retirementGermany(1990);
+
+function interviewQuestion2(job){
+  return function(name) {
+    if(job === 'designer'){
+      console.log(name + ', can you please what do you do');
+    }else if(job === 'teacher'){
+      console.log('What subject do you teach, ' + name + '?');
+    } else {
+      console.log('Hello ' + name + ', what do you do?');
+    }
+  }
+}
+
+interviewQuestion2('teacher', 'Mark');
